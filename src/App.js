2c9e5bfe74e76308f28268238c98415e7c1b77cc
LAPTOP-QIDO0NP0\Howard Mui <mhw19972013@gmail.com>
2021-10-29 02:36:55 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <p>Next design:</p>
+      <a href="https://www.youtube.com/watch?v=sKs9FiAHSN4&ab_channel=BrianDesign">
+        This is the Link
+      </a>
+    </div>
+  );
 }
 
 export default App;
